ae8a111357eb82f8c3c2d7ecb9a7658ff9ba907d
trung-nb <trungba53000@gmail.com>
2021-06-05 20:51:15 +0700

    test jenkins 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Hello World 2021 2022</h1>
+    <h1>Hello World 2021 2022 2023</h1>
   `
 })
 export class AppComponent {
