00f1fa57f55297463765df08f2f38b06814359ca
trung-nb <trungba53000@gmail.com>
2021-06-06 16:17:30 +0700

    update to 9.6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Hello World Angular 9.5</h1>
+    <h1>Hello World Angular 9.6</h1>
   `
 })
 export class AppComponent {
