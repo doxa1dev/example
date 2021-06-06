991ed97c5089003a122ea769d783cb5748f72cde
trung-nb <trungba53000@gmail.com>
2021-06-06 15:56:10 +0700

    update to 9.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Hello World Angular 9.0</h1>
+    <h1>Hello World Angular 9.2</h1>
   `
 })
 export class AppComponent {
